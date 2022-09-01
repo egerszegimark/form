8e11cddcbe1b637f05801761d386869f84a3777c
egerszegimark <72786848+egerszegimark@users.noreply.github.com>
2022-09-01 18:44:24 +0200

    creating

---
+++ form-app/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>valami</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>asd</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
